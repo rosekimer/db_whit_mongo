1a3a019e96116f308cf02b8ff6cf315f4c372eb0
Sar <samuel.alonso@people-media.com.mx>
2022-05-26 18:38:23 -0500

    se creo las validaciones y estatus de los datos

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
-const { request } = require('express');
 const User = require('../model/User');
+const {registerValidation,loginValidation} = require('../validation');
+
 
 router.post('/register', async (req,res) => {
+
+        // LEST VALICATE THE DATA BEFORE WE A USER
+        // const {error} = schema.validate(req.body); secuencia original
+        // res.send(error.details[0].message);
+
+        //const {error} = schema.validate(req.body);
+        const { error } = registerValidation(req.body);
+           if (error) return res.status(400).send(error.details[0].message);
+    
     const user = new User({
         name: req.body.name,
         email:req.body.email,
